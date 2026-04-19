bf3720e9aa7c4f129aa87f96a3598a001d7ac753
Yahay <yahay9356@gmail.com>
2026-04-19 21:36:25 +0200

    Add project files.

---
+++ ConsoleApplication51/ConsoleApplication51.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class first {
+private:
+	int x;
+public:
+	first() {
+		cout << "Hi iam c " << endl;
+	}
+};
+class second :public first {
+private:
+	int y;
+public:
+	second() {
+		cout << "Hi iam 2" << endl;
+	}
+};
+int main() {
+	//first op1;
+	second op2;
+}
